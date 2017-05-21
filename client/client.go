442f2e9995d0d3b026ad2b53bfa3b4cb397c86fd
Mathieu Lecarme <mlecarme@bearstech.com>
2017-05-21 22:13:37 +0200

    Asking for auth is now an unary filter

--- client/client.go
+++ client/client.go
@@ -4,11 +4,17 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/pkg/errors"
+	"github.com/skratchdot/open-golang/open"
 	"gitlab.bearstech.com/factory/gitlab-authenticated-rpc/client/auth"
 	"gitlab.bearstech.com/factory/gitlab-authenticated-rpc/client/conf"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"log"
 	"runtime"
+	"strings"
 )
 
 const (
@@ -33,7 +39,32 @@ func NewConn(domain string) (*grpc.ClientConn, error) {
 		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
 			InsecureSkipVerify: true, //FIXME don't do that on prod
 		})),
+		grpc.WithUnaryInterceptor(askForToken),
 		grpc.WithUserAgent(fmt.Sprintf("Journaleux %s #%s", runtime.GOOS, git_version)),
 	)
 	return conn, err
+}
+
+func askForToken(ctx context.Context, method string, req, resp interface{},
+	cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+
+	md := metadata.Pairs()
+	opts = append(opts, grpc.Trailer(&md))
+	err := invoker(ctx, method, req, resp, cc, opts...)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			log.Println(st)
+		}
+		u, ok := md["gar.auth_code_url"]
+		if ok {
+			if !strings.HasPrefix(u[0], "https://") {
+				log.Fatal("Bad url prefix, we all gonna die")
+			}
+			open.Run(u[0])
+		}
+		log.Fatalf("Can't hello: %v %v\n", err, md)
+		return err
+	}
+	return nil
 }
--- garc.go
+++ garc.go
@@ -3,17 +3,12 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 
 	"golang.org/x/net/context"
 
 	"github.com/golang/protobuf/ptypes/empty"
-	"github.com/skratchdot/open-golang/open"
 	"gitlab.bearstech.com/factory/gitlab-authenticated-rpc/client/client"
 	"gitlab.bearstech.com/factory/gitlab-authenticated-rpc/rpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
 const (
@@ -35,32 +30,20 @@ func main() {
 	h := rpc.NewHelloServiceClient(conn)
 
 	ctx := context.Background()
-	md := metadata.Pairs()
 
-	hello, err := h.SayHello(ctx, &rpc.HelloRequest{os.Args[2]}, grpc.Trailer(&md))
+	hello, err := h.SayHello(ctx, &rpc.HelloRequest{os.Args[2]})
 	if err != nil {
-		st, ok := status.FromError(err)
-		if ok {
-			log.Println(st)
-		}
-		u, ok := md["gar.auth_code_url"]
-		if ok {
-			if !strings.HasPrefix(u[0], "https://") {
-				log.Fatal("Bad url prefix, we all gonna die")
-			}
-			open.Run(u[0])
-		}
-		log.Fatalf("Can't hello: %v %v\n", err, md)
+		log.Fatalf("Can't hello: %v\n", err)
 	}
 	log.Printf("Hello: %s\n", hello)
-	hello, err = h.SayHello(ctx, &rpc.HelloRequest{"Super " + os.Args[2]}, grpc.Trailer(&md))
+	hello, err = h.SayHello(ctx, &rpc.HelloRequest{"Super " + os.Args[2]})
 	if err != nil {
-		log.Fatalf("Can't hello: %v %v\n", err, md)
+		log.Fatalf("Can't hello: %v\n", err)
 	}
 	log.Printf("Super Hello: %s\n", hello)
-	u, err := h.WhoAmI(ctx, &empty.Empty{}, grpc.Trailer(&md))
+	u, err := h.WhoAmI(ctx, &empty.Empty{})
 	if err != nil {
-		log.Fatalf("Can't who am I: %v %v\n", err, md)
+		log.Fatalf("Can't who am I: %v \n", err)
 	}
 	log.Printf("Who am I?: %v\n", u)
 
