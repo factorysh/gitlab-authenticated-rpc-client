9c81d9c3f2c1f8d62c86e2697b5d4531184bda81
Mathieu Lecarme <mlecarme@bearstech.com>
2018-04-15 15:24:24 +0200

    Fix: ClientConn. CA from ENV is not handled here

--- command/command.go
+++ command/command.go
@@ -2,10 +2,8 @@ package command
 
 import (
 	"gitlab.bearstech.com/factory/gitlab-authenticated-rpc/client/client"
-	"gitlab.bearstech.com/factory/gitlab-authenticated-rpc/client/conf"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"os"
 )
 
 type Client struct {
@@ -20,12 +18,7 @@ func NewClient() *Client {
 }
 
 func (c *Client) SetDomain(domain string) (err error) {
-	ca, err := conf.GetCA(os.Getenv("CA_PATH"))
-	if err != nil {
-		return err
-	}
 	cc := client.New(domain)
-	cc.CertPool = ca
-	c.Conn, err = cc.ClientConn()
+	c.Conn, err = cc.ClientConn(true)
 	return err
 }
